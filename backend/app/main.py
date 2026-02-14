617a1471bf318a2567182e4686d0ac334c91db15
vivekanandkumawat261 <23f2004577@ds.study.iitm.ac.in>
2026-02-14 18:02:36 +0530

    changed cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://task-forge-44y1-9kfpfnhpl.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
