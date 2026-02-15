e63b3476fa2447435aed7e5e0ce1c1b123ab396c
vivekanandkumawat261 <23f2004577@ds.study.iitm.ac.in>
2026-02-15 19:26:17 +0530

    correction

--- frontend-starter/src/api/apiClient.js
+++ frontend-starter/src/api/apiClient.js
@@ -5,8 +5,8 @@ import axios from "axios";
  */
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json"
